db5645aaec5a8ddc15a0b378a8fbe0d4da89d140
aarthigopinath19 <aarthiravi19@gmail.com>
2023-05-16 13:47:30 +0530

    project structure

---
+++ src/git/firstproject.java
@@ -0,0 +1,16 @@
+package git;
+
+public class firstproject {
+
+	
+	public static void main(String[] args) {
+		
+		
+		 int a,b,c; 
+		 a=10;
+		 b=10;
+		 c=a*b;
+		 System.out.println("The final product is"+c);
+		 
+	}
+}
